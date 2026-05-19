7915ab23e796032ffb9d1ebe406e33b1b00b08f0
tsaiyihsiu-star <tsaiyihsiu@gmail.com>
2026-05-19 18:08:30 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'todo-v368';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://cdn.jsdelivr.net/npm/idb-keyval@6/dist/umd/index.js'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting(); 
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[Service Worker] 正在快取所有資源');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[Service Worker] 刪除舊快取:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
